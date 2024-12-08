b42630f95fa26c61d61ce5da561dd3f775d013d9
aakashdeveloper <ahanda205@gmail.com>
2024-12-08 09:25:43 +0530

    reactapp

---
+++ firstapp/src/component/App.js
@@ -0,0 +1,18 @@
+import React,{Fragment} from 'react';
+import Home from './Home';
+import Header from './Header'
+
+
+//JSX >JavaScript Xml
+function App(){
+
+    return(
+        <Fragment>
+            <Header/>
+            <Home/>
+        </Fragment>
+        
+    )
+}
+
+export default App
---
+++ firstapp/src/component/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = ( ) => {
+
+
+    return(
+        <header> 
+            <div className="logo">React</div>
+            <input/>
+            <div id="userText">User Text Here</div>
+            <hr/>
+        </header>
+    )
+}
+
+export default Header
